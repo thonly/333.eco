06613a28a2bea9de72dbba3fea6074f71a940fa3
Thon Ly <thonly@heartbank.ceo>
2022-12-29 09:41:41 -0800

    refactor

--- components/e3-footer/e3-sponsors/template.mjs
+++ components/e3-footer/e3-sponsors/template.mjs
@@ -2,7 +2,7 @@ import { ORIGIN, THONLY } from "/global.mjs";
 const template = document.createElement("template");
 
 template.innerHTML = `
-    <link rel="stylesheet" href="${THONLY}/components/tl-footer/shadow.css">
+    <link rel="stylesheet" href="${THONLY}/components/tl-footer/tl-donors/shadow.css">
     <link rel="stylesheet" href="${ORIGIN}/components/e3-footer/e3-sponsors/shadow.css">
     <h5>Sponsors</h5>
     <ul class="donors">
