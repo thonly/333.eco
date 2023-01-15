08a4f6d9820bbf39cc407c1326abef285070602a
Thon Ly <thonly@heartbank.ceo>
2023-01-15 09:16:55 -0800

    refactor

--- components/e3-header/template.mjs
+++ components/e3-header/template.mjs
@@ -63,8 +63,8 @@ template.innerHTML = `
             <select onchange="this.getRootNode().host.changeLanguage(event)">
                 <option value="en">English</option>
                 <option value="km" disabled>Khmer</option>
-                <option value="es" disabled>Spanish</option>
                 <option value="ru" disabled>Russian</option>
+                <option value="es" disabled>Spanish</option>
                 <option value="zh" disabled>Chinese</option>
             </select>
         </aside>
